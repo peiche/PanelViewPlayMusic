3ad2b9352c6f63254f68f16f06ab533b8f27a2f5
Paul <peiche@gmail.com>
2014-01-08 00:49:05 -0600

    AW YISS.

--- js/background.js
+++ js/background.js
@@ -8,7 +8,7 @@ chrome.browserAction.onClicked.addListener(function() {
 			url: 'https://play.google.com/music/',
 			type: 'panel',
 			focused: true,
-			width: 425
+			width: 428
 		}, function(window) {
 			MUSIC_WINDOW_ID = window.id;
 		});
@@ -23,20 +23,4 @@ chrome.windows.onRemoved.addListener(function(windowId) {
 	if (windowId == MUSIC_WINDOW_ID) {
 		MUSIC_WINDOW_ID = 0;
 	}
-});
-
-var id = '';
-var options = {
-	type: 'basic',
-	title: 'title',
-	message: 'message',
-	iconUrl: '/img/icon_16.png'
-};
-chrome.notifications.create(id, options, function(notificationId) {
-	console.log(notificationId);
-});
-
-// handle message passing
-chrome.extension.onRequest.addListener(function(request, sender) {
-	chrome.tabs.update(sender.tab.id, {url: request.redirect});
 });
